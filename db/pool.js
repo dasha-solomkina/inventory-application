c65a71b809d2648718781111ce3a37a968153bdb
darialiit <solomkina.daria@gmail.com>
2024-09-05 16:26:12 -0300

    test

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
-console.log('Database URL:', process.env.DATABASE_URL)
+
 const { Pool } = require('pg')
 
 module.exports = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  host: 'meticulous-empathy.railway.internal',
+  user: 'postgres',
+  database: 'railway',
+  password: 'rGLsYgqbNOpVGPluWMDNxaNdFjigfHrt',
+  port: 5432,
 })
