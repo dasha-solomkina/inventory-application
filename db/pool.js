eba5350e90e0f9a153fd0ab39eb53b3cf59d10da
darialiit <solomkina.daria@gmail.com>
2024-09-05 16:10:48 -0300

    update env

--- db/pool.js
+++ db/pool.js
@@ -5,6 +5,6 @@ const { Pool } = require('pg')
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // Required for Railway's SSL configuration
+    rejectUnauthorized: false,
   },
 })
